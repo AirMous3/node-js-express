ff921a3846b5dbc7d9092f0c65c8421817b0f705
AirMouse <airmouselol@gmail.com>
2022-09-26 13:37:59 +0300

    feature: add controller and CRUD operations

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import Post from './Post.js';
+import router from './router.js';
 
 const PORT = 5000;
 const DATA_BASE_URL = `mongodb+srv://user:user@cluster0.ktdg5ds.mongodb.net/?retryWrites=true&w=majority`;
 
 const app = express();
 app.use(express.json());
-
-app.post('/', async (req, res) => {
-    console.log(req.body);
-    try {
-        const {author, title, content} = req.body;
-        const post = await Post.create({author, title, content});
-        res.json(post);
-    } catch (e) {
-        res.status(500).json(e);
-    }
-});
-
-app.get('/', (req, res) => {
-    res.status(200).json('server is working');
-});
+app.use('/api', router);
 
 async function startApp() {
     try {
